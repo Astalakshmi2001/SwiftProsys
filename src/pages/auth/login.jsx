bd5c43f57cc1b08fb3ae765f414b603ae82a50a0
Astalakshmi <2019astalakshmi@gmail.com>
2025-05-18 22:35:02 +0100

    Login and Signup UI Update

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import '../../App.css'
+import { Colors } from '../../constant/colors'
 
 function Login() {
   return (
     <div>
-      <h1>Login</h1>
-      <input type="text" />
-      <input type="password" name="" id="" />
-      <button>LOGiN</button>
+      <div className='container-fluid d-flex p-0'>
+      <div className="imagecontainer" style={{ backgroundColor: Colors.primary, width: '60%', height: '100vh' }}>
+
+      </div>
+      <div className="contentcontainer">
+        <div className='formcontainer'>
+          <h3>Login</h3>
+          <div className='input-field'>
+            <input type="text" id='username' required />
+            <label htmlFor="username">Username</label>
+          </div>
+
+          <div className='input-field' style={{marginBottom: 5}}>
+            <input type="password" id='password' required />
+            <label htmlFor="password">Password</label>
+          </div>
+          <a href="" style={{alignSelf: 'flex-end'}}>Forgot Password</a>
+          <button>Login</button>
+          <p>don't have an account? <a href="/signup">Signup</a></p>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
