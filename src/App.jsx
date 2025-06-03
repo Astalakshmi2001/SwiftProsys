79f3878301a5d3d9a5dd59acf4b06160577fbe38
Nandhakumar <1it21nandhakumar@gmail.com>
2025-06-03 22:14:00 +0530

    Login function updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import Login from './pages/auth/login'
 import Signup from './pages/auth/signup'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate  } from 'react-router-dom';
+import ProtectedRoute from './components/protectedRoute';
 import Production_report from './pages/Production/production_report';
 import UserScreen from './screens/userScreen';
 import Attendance from './pages/attendance/dailyattendance';
@@ -10,34 +11,33 @@ import EmployeeList from './pages/attendance/empList';
 import AdminScreen from './screens/adminScreen';
 
 function App() {
-  const role = "admin";
-
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/Production" element={<Production_report />} />
-        {role === "user" ? (
-          <>
-            {/* <Route path="/userscreen" element={<UserScreen />} /> */}
-            <Route path="/" element={<UserScreen />}>
-              <Route index element={<Attendance />} />
-            </Route>
-          </>
-        ) : role === "admin" ? (
-          <>
-            {/* <Route path="/adminscreen" element={<AdminScreen />} /> */}
-            <Route path="/" element={<AdminScreen />}>
-              <Route path="/emplist" element={<EmployeeList />} />
-              <Route path="/addemployee" element={<AddEmployee />} />
-            </Route>
-          </>
-        ) : (
-          <Route path="/" element={<Login />} />
-        )}
+
+        {/* User routes protected */}
+        <Route element={<ProtectedRoute roleAllowed="user" />}>
+          <Route path="/" element={<UserScreen />}>
+            <Route index element={<Attendance />} />
+          </Route>
+        </Route>
+
+        {/* Admin routes protected */}
+        <Route element={<ProtectedRoute roleAllowed="admin" />}>
+          <Route path="/" element={<AdminScreen />}>
+            <Route path="emplist" element={<EmployeeList />} />
+            <Route path="addemployee" element={<AddEmployee />} />
+          </Route>
+        </Route>
+
+        {/* Catch-all: redirect unknown routes to login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App
