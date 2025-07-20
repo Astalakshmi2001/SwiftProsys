a74fb5b97794cac4ad7a6f1e60e04e9a04310472
Astalakshmi <2019astalakshmi@gmail.com>
2025-07-20 12:24:39 +0530

    Firebase account change and attendance report update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SwiftProsys/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1500, // optional
     rollupOptions: {
